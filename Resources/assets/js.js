0896d4e08ab57de4ff1641ac3e715be238ae65ef
sofiaysita <sofiaysita3@gmail.com>
2022-12-19 18:50:54 -0600

    Dashboard updated with map and gantt chart

--- Resources/assets/js.js
+++ Resources/assets/js.js
@@ -1,41 +1,19 @@
     // Specify data
-    const continentData = [
+    const countryData = [
       {
-        id: "NA",
-        label: "North America",
-        value: "16.3",
-        showLabel: "1"
-  
+        id: "143",
+        value: "-1500",
+        link: "https://www.google.com/maps"
       },
       {
-        id: "SA",
-        label: "South America",
-        value: "12.0",
-        showLabel: "1"
+        id: "158",
+        value: "-500",
+        link: "https://www.google.com/maps"
       },
       {
-        id: "AS",
-        label: "Asia",
-        value: "30.0",
-        showLabel: "1"
-      },
-      {
-        id: "EU",
-        label: "Europe",
-        value: "6.7",
-        showLabel: "1"
-      },
-      {
-        id: "AF",
-        label: "Africa",
-        value: "20.3",
-        showLabel: "1"
-      },
-      {
-        id: "AU",
-        label: "Australia",
-        value: "5.2",
-        showLabel: "1"
+        id: "53",
+        value: "-700",
+        link: "https://www.google.com/maps"
       }
     ];
   
@@ -67,16 +45,69 @@
       dataFormat: "json", 
       dataSource: {
         chart: {
-          caption: "Percentage of Land Area on Planet Earth",
-          subCaption: "Data Source: www.enchantedlearning.com",
-          xAxisName: "Continent", 
-          yAxisName: "% Land Area", 
-          numberSuffix: "%",
-          theme: "fusion"   
-  
+          dateformat: "dd/mm/yyyy",
+          caption: "God Regions",
+          theme: "fusion",
+          canvasborderalpha: "40",
+          ganttlinealpha: "50"
         },
-        // Connect the data
-        data: continentData
+        tasks: {
+          color: "#5D62B5",
+          task: [
+            {
+              start: "01/01/0001",
+              end: "01/01/3970"
+            },
+            {
+              start: "01/01/3100",
+              end: "01/01/3700"
+            },
+            {
+              start: "01/01/4700",
+              end: "01/01/5100"
+            }
+          ]
+        },
+        processes: {
+          headertext: "Mythology",
+          headeralign: "left",
+          fontsize: "14",
+          isbold: "0",
+          align: "left",
+          process: [
+            {
+              label: "Egypt"
+            },
+            {
+              label: "Greek"
+            },
+            {
+              label: "Norse"
+            }
+          ]
+        },
+        categories: [
+          {
+
+            category: [
+              {
+                start: "01/01/0001",
+                end: "01/01/3100",
+                label: "4000 - 900 BC"
+              },
+              {
+                start: "01/01/3100",
+                end: "01/01/3700",
+                label: "900 - 300 BC"
+              },
+              {
+                start: "01/01/3700",
+                end: "01/01/5100",
+                label: "300 BC - 1100 AD"
+              }
+            ]
+            }
+            ]
       }
     }; 
 
@@ -106,16 +137,38 @@
       dataFormat: "json", 
       dataSource: {
         chart: {
-          caption: "Percentage of Land Area on Planet Earth",
-          subCaption: "Data Source: www.enchantedlearning.com",
-          xAxisName: "Continent", 
-          yAxisName: "% Land Area", 
-          numberSuffix: "%",
-          theme: "fusion"   
+          caption: "Godly Regions",
+          subcaption: "Click on a region",
+          labelsepchar: ": ",
+          entitytooltext: "$lname: From year <b>$datavalue </b>",
+          entityfillhovercolor: "#FFF9C4",
+          theme: "fusion"
   
         },
+      colorrange: {
+        minvalue: "-1500",
+        code: "#FFE0B2",
+        gradient: "10",
+        color: [
+          {
+            minvalue: "-1500",
+            maxvalue: "-1000",
+            color: "#FFD74D"
+          },
+          {
+            minvalue: "-1000",
+            maxvalue: "-500",
+            color: "#FB8C00"
+          },
+          {
+            minvalue: "0",
+            maxvalue: "1000",
+            color: "#E65100"
+          }
+        ]
+      },
         // Connect the data
-        data: continentData
+        data: countryData
       }
     }; 
       
@@ -127,14 +180,14 @@
       }
   
       function renderLine(){
-        chartConfig.type = 'line';   	
+        chartConfig.type = 'gantt';   	
           chartConfig.renderAt = 'line-chart-container';        
         var fusioncharts = new FusionCharts(chartConfig);
         fusioncharts.render();
       }    
   
       function renderMap(){
-          chartConfig3.type = 'world';     
+          chartConfig3.type = 'maps/worldwithcountries';     
           chartConfig3.renderAt = 'map-container';         
           var fusioncharts = new FusionCharts(chartConfig3);
           fusioncharts.render();
